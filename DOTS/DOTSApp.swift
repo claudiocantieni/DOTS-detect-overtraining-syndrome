5cb523a92b601847158ba212a4c985389bedd69d
Claudio Cantieni <claudiocantieni@icloud.com>
2022-04-03 18:46:02 +0200

    Initial Commit

---
+++ DOTS/DOTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOTSApp.swift
+//  DOTS
+//
+//  Created by Claudio Cantieni on 03.04.22.
+//
+
+import SwiftUI
+
+@main
+struct DOTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
